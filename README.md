49254d0a7ee30020e0c616a2e0a2b34007c75685
Max <tnalsl1984@naver.com>
2018-11-02 14:37:49 +0900

    [ init ] - [ 초기설정 ] [    프로젝트 초기 설정 ]

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringBoot-MemberShip-Core
+SpringBoot MemberShip Core Project ( OAuth2 + SSO + JPA(Hibernate) + DATARest ) Server
---
+++ src/main/java/kr/max/core/membership/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kr.max.core.membership;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+  
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(MembershipApplication.class);
+  }
+  
+}
